c3af12c2f2e04141c37fc552756bb717acdae584
chrsut01 <chrissu360@gmail.com>
2022-12-02 15:25:05 +0100

    attempt3

--- src/main/java/com/example/mytunesproject/Controller.java
+++ src/main/java/com/example/mytunesproject/Controller.java
@@ -2,12 +2,33 @@ package com.example.mytunesproject;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 
 
 public class Controller {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    @FXML
+    private TextField insertArtist;
+
+    @FXML
+    private TextField insertFile;
+
+    @FXML
+    private TextField insertTime;
+
+    @FXML
+    private TextField insertTitle;
 
     @FXML
     private TextField filterField;
@@ -16,7 +37,10 @@ public class Controller {
     private TextField songIsPlaying;
 
     @FXML
-    private Slider volumeConttol;
+    private Slider volumeControl;
+
+    @FXML
+    private TextField playlistName;
 
     @FXML
     void addSongPlaylist(ActionEvent event) {
@@ -34,23 +58,61 @@ public class Controller {
     }
 
     @FXML
-    void deletePlayList(ActionEvent event) {
-
-    }
-
-    @FXML
     void deleteSongfromPlayList(ActionEvent event) {
 
     }
 
-    @FXML
-    void editPlayList(ActionEvent event) {
+    /*  // Dialogen defineres med "ButtonTyes", hvilket betyder at vi kan bestemmer at
+        // dialogen afsluttes med at vi tester på knapperne
+        Dialog<ButtonType> dialog = new Dialog();
 
+        // Her sættes vinduet op
+        dialog.setTitle("Eksempel på dialog");
+        dialog.setHeaderText("Her kan stå noget tekst");
+        dialog.getDialogPane().getButtonTypes().addAll(ButtonType.OK, ButtonType.CANCEL);
+        TextField felt1 = new TextField();
+        TextField felt2 = new TextField();
+        VBox box = new VBox(felt1, felt2);
+        dialog.getDialogPane().setContent(box);
+         // Her afsluttes dialogen med at man kan trykke på OK
+        Optional<ButtonType> knap = dialog.showAndWait();
+        // Derefter kan vi henter felternes indhold ud og gøre hvad der skal gøres...
+        if (knap.get() == ButtonType.OK)
+            System.out.println("Felt1 = " + felt1.getText() + " felt2 = " + felt2.getText());
+    }*/
+
+    @FXML
+    void newPlayList(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("NewEditPlaylist.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
     }
 
     @FXML
-    void editSongLib(ActionEvent event) {
+    void editPlayList(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("NewEditPlaylist.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+    }
+    @FXML
+    void deletePlayList(ActionEvent event) {    }
 
+    @FXML
+    void newSongLib(ActionEvent event) throws IOException{
+        root = FXMLLoader.load(getClass().getResource("NewEditSong.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+    }
+
+    @FXML
+    void editSongLib(ActionEvent event) throws IOException{
+        root = FXMLLoader.load(getClass().getResource("NewEditSong.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
     }
 
     @FXML
@@ -68,15 +130,9 @@ public class Controller {
 
     }
 
-    @FXML
-    void newPlayList(ActionEvent event) {
 
-    }
 
-    @FXML
-    void newSonglib(ActionEvent event) {
 
-    }
 
     @FXML
     void play(ActionEvent event) {
@@ -92,17 +148,7 @@ public class Controller {
     void search(ActionEvent event) {
 
     }
-    @FXML
-    private TextField insertArtist;
 
-    @FXML
-    private TextField insertFile;
-
-    @FXML
-    private TextField insertTime;
-
-    @FXML
-    private TextField insertTitle;
 
     @FXML
     void cancelSongInfo(ActionEvent event) {
@@ -124,8 +170,7 @@ public class Controller {
 
     }
 
-    @FXML
-    private TextField playlistName;
+
 
     @FXML
     void cancelEdit(ActionEvent event) {
